5f566e8a2f89cc903e743252f33584ecc850de7f
Caroline Jepps <carolinejepps@android-596c691a2d1213bc.localdomain>
2020-08-05 14:06:50 -0700

    Updated application to include schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,9 @@
 
 import React, { useState } from "react";
-
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment/Index";
+
 const days = [
   {
     id: 1,
@@ -21,8 +22,74 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "10am",
+    interview: {
+      student: "Bob Bobertson",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "4pm",
+    interview: {
+      student: "Jennifer Blueberry",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "9am",
+    interview: {
+      student: "Harry Potter",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  }
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const schedule = appointments.map(appointment => {
+
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+      />
+    );
+  });
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -40,8 +107,7 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-        {
-        }
+        {schedule}
       </section>
     </main>
   );
